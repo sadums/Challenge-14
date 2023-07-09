ab9a5ed3ad7d741a369cf55e811197e3bad49a34
sadums <samjadams4@gmail.com>
2023-07-09 12:39:35 -0600

    created controller functions

---
+++ src/controllers/thoughtController.js
@@ -0,0 +1,59 @@
+module.exports={
+    async getThoughts (req, res) {
+        try{
+
+        }catch(e){
+            console.error(e);
+            res.status(500).json(e);
+        }
+    },
+    async getOneThought (req, res) {
+        try{
+
+        }catch(e){
+            console.error(e);
+            res.status(500).json(e);
+        }
+    },
+    async newThought (req, res) {
+        try{
+
+        }catch(e){
+            console.error(e);
+            res.status(500).json(e);
+        }
+    },
+    async updateThought (req, res) {
+        try{
+
+        }catch(e){
+            console.error(e);
+            res.status(500).json(e);
+        }
+    },
+    async deleteThought (req, res) {
+        try{
+
+        }catch(e){
+            console.error(e);
+            res.status(500).json(e);
+        }
+    },
+    async newReaction (req, res) {
+        try{
+
+        }catch(e){
+            console.error(e);
+            res.status(500).json(e);
+        }
+    },
+    async deleteReaction (req, res) {
+        try{
+
+        }catch(e){
+            console.error(e);
+            res.status(500).json(e);
+        }
+    },
+
+} 
---
+++ src/controllers/userController.js
@@ -0,0 +1,58 @@
+module.exports = {
+  // Get all users, GET
+  async getUsers(req, res) {
+    try {
+    } catch (e) {
+      console.error(e);
+      res.status(500).json(e);
+    }
+  },
+  //   Get a single user, GET
+  async getOneUser(req, res) {
+    try {
+    } catch (e) {
+      console.error(e);
+      res.status(500).json(e);
+    }
+  },
+  //   Create a new user, POST
+  async newUser(req, res) {
+    try {
+    } catch (e) {
+      console.error(e);
+      res.status(500).json(e);
+    }
+  },
+  //   Update a user, PUT
+  async updateUser(req, res) {
+    try {
+    } catch (e) {
+      console.error(e);
+      res.status(500).json(e);
+    }
+  },
+  //   Delete a user, DELETE
+  async deleteUser(req, res) {
+    try {
+    } catch (e) {
+      console.error(e);
+      res.status(500).json(e);
+    }
+  },
+  //   Add a friend to a user's friend list, POST
+  async newFriend(req, res) {
+    try {
+    } catch (e) {
+      console.error(e);
+      res.status(500).json(e);
+    }
+  },
+  //   Delete a friend from a user's friend list, DELETE
+  async deleteFriend(req, res) {
+    try {
+    } catch (e) {
+      console.error(e);
+      res.status(500).json(e);
+    }
+  },
+};
