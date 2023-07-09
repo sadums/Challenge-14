711204a4c5e0efe7dc4d89153219940cfaaa9842
sadums <samjadams4@gmail.com>
2023-07-09 12:43:21 -0600

    adding comments to controller routes

--- src/controllers/thoughtController.js
+++ src/controllers/thoughtController.js
@@ -1,59 +1,58 @@
-module.exports={
-    async getThoughts (req, res) {
-        try{
-
-        }catch(e){
-            console.error(e);
-            res.status(500).json(e);
-        }
-    },
-    async getOneThought (req, res) {
-        try{
-
-        }catch(e){
-            console.error(e);
-            res.status(500).json(e);
-        }
-    },
-    async newThought (req, res) {
-        try{
-
-        }catch(e){
-            console.error(e);
-            res.status(500).json(e);
-        }
-    },
-    async updateThought (req, res) {
-        try{
-
-        }catch(e){
-            console.error(e);
-            res.status(500).json(e);
-        }
-    },
-    async deleteThought (req, res) {
-        try{
-
-        }catch(e){
-            console.error(e);
-            res.status(500).json(e);
-        }
-    },
-    async newReaction (req, res) {
-        try{
-
-        }catch(e){
-            console.error(e);
-            res.status(500).json(e);
-        }
-    },
-    async deleteReaction (req, res) {
-        try{
-
-        }catch(e){
-            console.error(e);
-            res.status(500).json(e);
-        }
-    },
-
-} 
+module.exports = {
+  // Get all thoughts, GET
+  async getThoughts(req, res) {
+    try {
+    } catch (e) {
+      console.error(e);
+      res.status(500).json(e);
+    }
+  },
+  // Get a single thought, GET
+  async getOneThought(req, res) {
+    try {
+    } catch (e) {
+      console.error(e);
+      res.status(500).json(e);
+    }
+  },
+  // Create a new thought, POST
+  async newThought(req, res) {
+    try {
+    } catch (e) {
+      console.error(e);
+      res.status(500).json(e);
+    }
+  },
+  // Update a thought, PUT
+  async updateThought(req, res) {
+    try {
+    } catch (e) {
+      console.error(e);
+      res.status(500).json(e);
+    }
+  },
+  // Delete a thought, DELETE
+  async deleteThought(req, res) {
+    try {
+    } catch (e) {
+      console.error(e);
+      res.status(500).json(e);
+    }
+  },
+  // Create a new reaction, POST
+  async newReaction(req, res) {
+    try {
+    } catch (e) {
+      console.error(e);
+      res.status(500).json(e);
+    }
+  },
+  // Delete a reaction, DELETE
+  async deleteReaction(req, res) {
+    try {
+    } catch (e) {
+      console.error(e);
+      res.status(500).json(e);
+    }
+  },
+};
